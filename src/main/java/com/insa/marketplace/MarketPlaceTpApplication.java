568c1864f0867963810ccfb569e6003855c2b56b
Marius Franck <mariusfranck20@gmail.com>
2025-12-03 14:47:59 +0100

    init push

---
+++ src/main/java/com/insa/marketplace/MarketPlaceTpApplication.java
@@ -0,0 +1,13 @@
+package com.insa.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceTpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketPlaceTpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/insa/marketplace/MarketPlaceTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insa.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceTpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
